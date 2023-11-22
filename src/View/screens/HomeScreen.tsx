86b90c45fa6ae56453212b34935183528907ed14
Pedro Pamplona <84392638+pcppamplona@users.noreply.github.com>
2023-11-21 21:44:34 -0300

    atualização

--- src/View/screens/HomeScreen.tsx
+++ src/View/screens/HomeScreen.tsx
@@ -1,33 +1,57 @@
-import React, { useEffect } from "react";
-import { View, Text, Button } from "react-native";
-import { useRoute } from "@react-navigation/native"; 
-import { usuarioSessao } from './LoginScreen';
+import React, { useEffect, useState } from "react";
+import { View, Text, Button, Image, TouchableOpacity } from "react-native";
 import proximaVacinaController from "../../Controller/proximaVacinaController";
 import DependenteController from "../../Controller/DependenteController";
 import * as Notifications from "expo-notifications";
+import { usuarioSessao } from "./LoginScreen";
+import Icon from "react-native-vector-icons/Fontisto";
+import Icon2 from "react-native-vector-icons/FontAwesome";
+import UsuarioController from "../../Controller/UsuarioController";
 
-export function HomeScreen() {
+export function HomeScreen({ navigation }) {
   // const route = useRoute();
   // const { usuarioId } = route.params as { usuarioId: number | null };
   useEffect(() => {
-    proximaVacina()
-  }, [])
+    proximaVacina();
+  }, []);
 
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    getName();
+  }, []);
+
+  async function getName() {
+    try {
+      const nome = await UsuarioController.getUsuarioPorId(usuarioSessao);
+      setName(nome.Nome_Completo);
+    } catch (error) {
+      throw error;
+    }
+  }
 
   async function proximaVacina() {
     let dependentesAtualizados = 0;
     let informacoesDependentes = [];
-  
+
     try {
-      const dependentes = await DependenteController.buscarDependentes(usuarioSessao);
-  
+      const dependentes = await DependenteController.buscarDependentes(
+        usuarioSessao
+      );
+
       for (const dependente of dependentes) {
-        console.log(`ID do dependente: ${dependente.id_dependente}| Data de nascimento: ${dependente.Data_Nascimento}| Nome: ${dependente.Nome_Dependente}`);
-        
-        const dependentesProximaVacina = await proximaVacinaController.verificacaoProxVacina(dependente.id_dependente, dependente.Data_Nascimento);
-        
+        console.log(
+          `ID do dependente: ${dependente.id_dependente}| Data de nascimento: ${dependente.Data_Nascimento}| Nome: ${dependente.Nome_Dependente}`
+        );
+
+        const dependentesProximaVacina =
+          await proximaVacinaController.verificacaoProxVacina(
+            dependente.id_dependente,
+            dependente.Data_Nascimento
+          );
+
         console.log(dependentesProximaVacina);
-        
+
         if (dependentesProximaVacina === false) {
           console.log("Não há atualizações de próximas vacinas!");
         } else {
@@ -38,27 +62,29 @@ export function HomeScreen() {
           informacoesDependentes.push({
             id_dependente: dependente.id_dependente,
             Nome_Dependente: dependente.Nome_Dependente,
-            proximasVacinas: await proximaVacinaController.buscarProxVacinas(dependente.id_dependente),
+            proximasVacinas: await proximaVacinaController.buscarProxVacinas(
+              dependente.id_dependente
+            ),
           });
         }
       }
       console.log("Dependentes atualizados: " + dependentesAtualizados);
       let notificacoesDependentes = [];
-      if(dependentesAtualizados > 0){
+      if (dependentesAtualizados > 0) {
         informacoesDependentes.forEach((dependenteInfo) => {
           const { Nome_Dependente, proximasVacinas } = dependenteInfo;
-      
+
           let notificacao = `${Nome_Dependente}:\n`;
-      
+
           proximasVacinas.forEach((proximaVacina) => {
             notificacao += `Vacina: ${proximaVacina.Nome_Vacina}\n`;
             notificacao += `Dose: ${proximaVacina.Dose}\n\n`;
           });
-      
+
           notificacoesDependentes.push(notificacao);
         });
-        agendarNotificacao(notificacoesDependentes)
-      }else{
+        agendarNotificacao(notificacoesDependentes);
+      } else {
         console.log("Não há atualizações de nenhum dependente");
       }
     } catch (error) {
@@ -66,10 +92,10 @@ export function HomeScreen() {
     }
   }
 
-  async function agendarNotificacao(notificacoesDependentes){
+  async function agendarNotificacao(notificacoesDependentes) {
     const trigger = new Date(Date.now());
     trigger.setSeconds(trigger.getSeconds() + 2);
-    
+
     await Notifications.scheduleNotificationAsync({
       content: {
         title: "Próximas doses",
@@ -86,11 +112,40 @@ export function HomeScreen() {
       shouldSetBadge: false,
     }),
   });
-  
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-red-700">Home</Text>
+    <View className="flex-1 bg-[#fff]">
+      <View className="flex flex-row items-center ml-4 mr-auto mt-10 mb-2">
+        <Image
+          source={require("../../../assets/logo.png")}
+          style={{ width: 32, height: 30 }}
+        />
+        <Text className="font-normal text-xl text-[#28282D]">Olá, </Text>
+
+        <Text className="font-bold text-xl text-[#28282D]">{name}</Text>
+        <View className="flex-1 flex-row justify-end">
+          <TouchableOpacity>
+            <Icon2
+              name="bell"
+              size={22}
+              color="#9e9ea0"
+              style={{ marginRight: 16 }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View className="h-[2px] w-11/12 ml-auto mr-auto bg-[#EAEAEA]" />
+      <Text style={{ color: "red" }}>Home</Text>
       <Text>ID do Usuário: {usuarioSessao}</Text>
+      <TouchableOpacity
+        onPress={() => {
+          console.log("HealthCenter pressed!");
+          navigation.navigate("HealthCenter");
+        }}
+      >
+        <Text>Posto de saúde</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- src/View/screens/VaccineScreen.tsx
+++ src/View/screens/VaccineScreen.tsx
@@ -1,10 +1,86 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  TextInput,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
+import { usuarioSessao } from "./LoginScreen";
+import ModalRemedio from "react-native-modal";
+import FormNewRemedio from "../components/FormNewRemedio";
+import FormEditRemedio from "../components/FormEditRemedio";
 export function VaccineScreen({ navigation }) {
+  const [vacinas, setVacinas] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
+
+  const [isSearchBarVisible, setIsSearchBarVisible] = useState(false);
+
+  const toggleSearchBar = () => {
+    setIsSearchBarVisible(!isSearchBarVisible);
+  };
+  const closeSearchBar = () => {
+    setIsSearchBarVisible(false);
+  };
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-red-700">Vacinas</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={() => closeSearchBar()}>
+      <View className="flex-1 flex-col bg-[#fff]">
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            bottom: 20,
+            right: 20,
+            zIndex: 1,
+            width: 40,
+            height: 40,
+            borderRadius: 100,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#2BB459",
+          }}
+        >
+          <Icon name="plus" size={22} color="white" />
+        </TouchableOpacity>
+
+        {vacinas.length === 0 ? (
+          <View>
+            <View className="ml-4 mr-auto mt-10 mb-10">
+              <Text className="font-bold text-2xl text-[#28282D]">
+                Vacinas
+              </Text>
+            </View>
+            <Image
+              source={require("../../../assets/")}
+              className="w-full h-auto top-[5%] bottom-0 animate-pulse"
+            />
+            <Text className="mt-10 ml-auto mr-auto line-clamp-2 items-center text-center text-lg text-[#1F9A55] font-bold">
+              Você ainda não possui nenhuma{"\n"} vacina cadastrado.
+            </Text>
+            <Text className="mt-14 ml-auto mr-auto line-clamp-2 items-center text-center text-sm text-[#282b29] font-roboto">
+              Para adicionar uma nova, clique no botão{" "}
+              <Text className="text-lg text-[#1F9A55] font-semibold">+</Text>
+            </Text>
+          </View>
+        ) : (
+          <>
+            <View className="flex flex-row w-11/12 justify-between items-center ml-4 mr-auto mt-10">
+              <Text className="font-bold text-2xl text-[#28282D]">
+                Medicamentos
+              </Text>
+              <TouchableOpacity onPress={() => toggleSearchBar()} className="">
+                <Icon name="filter" size={15} color="#2BB459" />
+              </TouchableOpacity>
+            </View>
+          </>
+        )}
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
